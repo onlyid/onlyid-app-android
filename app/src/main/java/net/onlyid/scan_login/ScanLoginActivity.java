0087e0b2e8baf24517c50180f66fc6d9da5beb70
ltb <liangtb@qq.com>
2020-08-14 21:21:19 +0800

    refactor: 使用camera x替代camera2

--- app/src/main/java/net/onlyid/scan_login/ScanLoginActivity.java
+++ app/src/main/java/net/onlyid/scan_login/ScanLoginActivity.java
@@ -1,35 +1,20 @@
 package net.onlyid.scan_login;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.graphics.ImageFormat;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CameraMetadata;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.params.StreamConfigurationMap;
-import android.media.Image;
-import android.media.ImageReader;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.Size;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.DecodeHintType;
@@ -43,39 +28,29 @@ import net.onlyid.Utils;
 import net.onlyid.databinding.ActivityScanLoginBinding;
 
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ScanLoginActivity extends AppCompatActivity {
-    static final String TAG = "ScanLoginActivity";
+    static final String TAG = ScanLoginActivity.class.getSimpleName();
     static final String[] PERMISSIONS = {
             Manifest.permission.CAMERA,
     };
     ActivityScanLoginBinding binding;
-    CameraManager cameraManager;
-    CameraCharacteristics characteristics;
-    CameraDevice cameraDevice;
-    ImageReader imageReader;
-    CameraCaptureSession captureSession;
     MultiFormatReader multiFormatReader;
-    HandlerThread decodeThread = new HandlerThread("decode");
-    Handler decodeHandler;
+    ExecutorService executorService;
 
     {
         multiFormatReader = new MultiFormatReader();
         Map<DecodeHintType, Object> hints = new EnumMap<>(DecodeHintType.class);
         hints.put(DecodeHintType.POSSIBLE_FORMATS, Collections.singletonList(BarcodeFormat.QR_CODE));
         hints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
-        hints.put(DecodeHintType.CHARACTER_SET, "utf-8");
         multiFormatReader.setHints(hints);
 
-        decodeThread.start();
-        decodeHandler = new Handler(decodeThread.getLooper());
+        executorService = Executors.newSingleThreadExecutor();
     }
 
     @Override
@@ -91,24 +66,7 @@ public class ScanLoginActivity extends AppCompatActivity {
             }
         }
 
-        binding.viewFinder.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                try {
-                    openCamera();
-                } catch (CameraAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-            }
-        });
+        startCamera();
     }
 
     @Override
@@ -122,129 +80,51 @@ public class ScanLoginActivity extends AppCompatActivity {
             }
         }
 
-        try {
-            openCamera();
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
+        startCamera();
     }
 
-    @SuppressLint("MissingPermission")
-    void openCamera() throws CameraAccessException {
-        cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
-
-        // 找到一个兼容的后置摄像头
-        String cameraId = null;
-        for (String id : cameraManager.getCameraIdList()) {
-            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(id);
-            int[] capabilities = characteristics.get(CameraCharacteristics.REQUEST_AVAILABLE_CAPABILITIES);
-            List<Integer> capabilityList = Arrays.stream(capabilities).boxed().collect(Collectors.toList());
-            if (!capabilityList.contains(CameraMetadata.REQUEST_AVAILABLE_CAPABILITIES_BACKWARD_COMPATIBLE))
-                continue;
-
-            if (characteristics.get(CameraCharacteristics.LENS_FACING) != CameraMetadata.LENS_FACING_BACK)
-                continue;
-
-            cameraId = id;
-        }
-        Log.d(TAG, "cameraId: " + cameraId);
-
-        // 打开摄像头
-        characteristics = cameraManager.getCameraCharacteristics(cameraId);
-        cameraManager.openCamera(cameraId, new CameraDevice.StateCallback() {
-            @Override
-            public void onOpened(@NonNull CameraDevice camera) {
-                Log.d(TAG, "onOpened, camera: " + camera);
-                cameraDevice = camera;
-                try {
-                    createCaptureSession();
-                } catch (CameraAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onDisconnected(@NonNull CameraDevice camera) {
-                Log.d(TAG, "onDisconnected: " + camera);
-            }
-
-            @Override
-            public void onError(@NonNull CameraDevice camera, int error) {
-                Log.e(TAG, "onError: " + error);
-            }
-        }, null);
-    }
-
-    /**
-     * 新建拍摄会话
-     */
-    void createCaptureSession() throws CameraAccessException {
-        StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-        Size[] sizes = map.getOutputSizes(ImageFormat.YUV_420_888);
-        Size size = Arrays.stream(sizes)
-                // 过滤掉2000以上的，不知为什么，太高分别率识别不到
-                .filter((s) -> s.getWidth() < 2000 && s.getHeight() < 2000)
-                // 取最大的
-                .max((a, b) -> a.getHeight() * a.getWidth() - b.getHeight() * b.getWidth())
-                .get();
-        Log.d(TAG, "sizes: " + Arrays.toString(sizes));
-        Log.d(TAG, "size: " + size);
-
-        imageReader = ImageReader.newInstance(size.getWidth(), size.getHeight(), ImageFormat.YUV_420_888, 2);
-        imageReader.setOnImageAvailableListener((reader) -> {
-            Image image = reader.acquireLatestImage();
-            if (image == null) return;
-
-            ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
-            byte[] data = new byte[byteBuffer.remaining()];
-            byteBuffer.get(data);
-
-            PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(data, image.getWidth(),
-                    image.getHeight(), 0, 0, image.getWidth(), image.getHeight(), false);
-            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+    void startCamera() {
+        ListenableFuture<ProcessCameraProvider> future = ProcessCameraProvider.getInstance(this);
+        future.addListener(() -> {
             try {
-                Result result = multiFormatReader.decode(bitmap);
-                String s = result.getText();
-                Log.d(TAG, "result: " + s);
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(binding.viewFinder.createSurfaceProvider());
 
-                captureSession.close();
-            } catch (NotFoundException e) {
-                // do nothing
-            } finally {
-                multiFormatReader.reset();
+                ImageAnalysis imageAnalysis = new ImageAnalysis.Builder().build();
+                imageAnalysis.setAnalyzer(executorService, (image) -> {
+                    ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
+                    byte[] data = new byte[byteBuffer.remaining()];
+                    byteBuffer.get(data);
+
+                    PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(data, image.getWidth(),
+                            image.getHeight(), 0, 0, image.getWidth(), image.getHeight(), false);
+                    BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+                    try {
+                        Result result = multiFormatReader.decode(bitmap);
+                        String s = result.getText();
+                        Log.d(TAG, "result: " + s);
+                    } catch (NotFoundException e) {
+                        // do nothing
+                    } finally {
+                        multiFormatReader.reset();
+                        image.close();
+                    }
+                });
+
+                ProcessCameraProvider cameraProvider = future.get();
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle(
+                        this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalysis);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            image.close();
-        }, decodeHandler);
-
-        List<Surface> outputs = new ArrayList<>();
-        outputs.add(imageReader.getSurface());
-        outputs.add(binding.viewFinder.getHolder().getSurface());
-        cameraDevice.createCaptureSession(outputs, new CameraCaptureSession.StateCallback() {
-            @Override
-            public void onConfigured(@NonNull CameraCaptureSession session) {
-                captureSession = session;
-                try {
-                    startPreview();
-                } catch (CameraAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                Log.e(TAG, "onConfigureFailed: " + session);
-            }
-        }, null);
+        }, ContextCompat.getMainExecutor(this));
     }
 
-    /**
-     * 开始预览
-     */
-    void startPreview() throws CameraAccessException {
-        CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-        builder.addTarget(binding.viewFinder.getHolder().getSurface());
-        builder.addTarget(imageReader.getSurface());
-        captureSession.setRepeatingRequest(builder.build(), null, null);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        executorService.shutdown();
     }
 }
