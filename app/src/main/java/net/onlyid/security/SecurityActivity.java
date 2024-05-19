2fa87a763abec6921d60df14ce1bae941a2ad193
ltb <liangtb@qq.com>
2024-05-19 12:10:47 +0800

    feat: 安全设置，完成SecurityActivity。

--- app/src/main/java/net/onlyid/security/SecurityActivity.java
+++ app/src/main/java/net/onlyid/security/SecurityActivity.java
@@ -1,9 +1,17 @@
 package net.onlyid.security;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 
+import net.onlyid.MyApplication;
 import net.onlyid.common.BaseActivity;
+import net.onlyid.common.Constants;
+import net.onlyid.common.MyHttp;
+import net.onlyid.common.Utils;
 import net.onlyid.databinding.ActivitySecurityBinding;
+import net.onlyid.entity.User;
+import net.onlyid.user_profile.EditAccountActivity;
 
 public class SecurityActivity extends BaseActivity {
     ActivitySecurityBinding binding;
@@ -13,5 +21,53 @@ public class SecurityActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySecurityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        initView();
+    }
+
+    void initView() {
+        User user = MyApplication.getCurrentUser();
+
+        binding.mobileTextView.setText(TextUtils.isEmpty(user.mobile) ? "-" : user.mobile);
+        binding.emailTextView.setText(TextUtils.isEmpty(user.email) ? "-" : user.email);
+
+        binding.mobileLayout.setOnClickListener(v -> mobile());
+        binding.emailLayout.setOnClickListener(v -> email());
+        binding.passwordLayout.setOnClickListener(v -> password());
+        binding.deviceLayout.setOnClickListener(v -> loginDevice());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        refresh();
+    }
+
+    void refresh() {
+        MyHttp.get("/user", (resp) -> {
+            Utils.pref.edit().putString(Constants.USER, resp).apply();
+            initView();
+        });
+    }
+
+    void mobile() {
+        Intent intent = new Intent(this, EditAccountActivity.class);
+        intent.putExtra("type", "mobile");
+        startActivity(intent);
+    }
+
+    void email() {
+        Intent intent = new Intent(this, EditAccountActivity.class);
+        intent.putExtra("type", "email");
+        startActivity(intent);
+    }
+
+    void password() {
+
+    }
+
+    void loginDevice() {
+        Intent intent = new Intent(this, LoginDeviceActivity.class);
+        startActivity(intent);
     }
 }
