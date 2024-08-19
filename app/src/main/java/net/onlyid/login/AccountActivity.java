73465d23b48ea1deed7d30386c3c89371360e31b
ltb <liangtb@qq.com>
2024-08-19 15:11:38 +0800

    feat: 上架整改，首次启动增加隐私政策弹窗。

--- app/src/main/java/net/onlyid/login/AccountActivity.java
+++ app/src/main/java/net/onlyid/login/AccountActivity.java
@@ -1,5 +1,6 @@
 package net.onlyid.login;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -10,16 +11,21 @@ import android.text.style.ClickableSpan;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.util.PatternsCompat;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import net.onlyid.R;
 import net.onlyid.common.BaseActivity;
 import net.onlyid.common.MyHttp;
 import net.onlyid.common.Utils;
 import net.onlyid.databinding.ActivityAccountBinding;
+import net.onlyid.databinding.DialogPrivacyBinding;
 import net.onlyid.entity.Entity1;
 
 public class AccountActivity extends BaseActivity {
@@ -64,6 +70,38 @@ public class AccountActivity extends BaseActivity {
         binding.textView.setText(ss);
         binding.submitButton.setOnClickListener((v) -> validateFields());
         editText = binding.accountInput.getEditText();
+
+        if (Utils.pref.getBoolean("showPrivacy", true)) showPrivacyDialog();
+    }
+
+    void showPrivacyDialog() {
+        DialogPrivacyBinding binding1 = DialogPrivacyBinding.inflate(getLayoutInflater());
+
+        SpannableString ss = new SpannableString("唯ID非常重视对个人信息的保护，为便于你理解《隐私政策》，现简要说明如下：");
+        ClickableSpan span = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                TermsActivity.start(AccountActivity.this, "privacy");
+            }
+        };
+        ss.setSpan(span, 22, 28, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        binding1.textView1.setMovementMethod(LinkMovementMethod.getInstance());
+        binding1.textView1.setText(ss);
+
+        AlertDialog dialog = new MaterialAlertDialogBuilder(this)
+                .setView(binding1.getRoot())
+                .setPositiveButton("同意，开始使用", (d, w) -> {
+                    Utils.pref.edit().putBoolean("showPrivacy", false).apply();
+                })
+                .setNeutralButton("不同意，退出", (d, w) -> {
+                    finish();
+                })
+                .show();
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setLetterSpacing(0);
+        Button neutralButton = dialog.getButton(DialogInterface.BUTTON_NEUTRAL);
+        neutralButton.setLetterSpacing(0);
+        neutralButton.setTextColor(getResources().getColor(R.color.text_secondary, null));
     }
 
     void validateFields() {
